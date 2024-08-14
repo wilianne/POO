41472508ddc3eb0e6643f7424619dc171076d8b5
Wilianne Paixao <williannequaresmapaixao@gmail.com>
2024-08-14 00:59:45 -0300

    lessons

---
+++ Language python/lesson1/concepts.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+print("POO")
+
+#string -> text
+name = "Wilianne Quaresma Paixao"
+
+#integer -> number
+age = 20
+age = 32
+
+print(name)
+print(age)
+
+nameTyped = input("Please, enter a name: ")
+print(f"The name entered was: {nameTyped}")
---
+++ Language python/lesson1/operators.py
@@ -0,0 +1,28 @@
+n1 = 7
+n2 = 9
+aprovado = True
+media = 6
+minimo = 7
+
+print("Arithmetic Operators")
+print("Sum: ",n1+n2)
+print("Subtraction: ",n1-n2)
+print("Multiplication: ",n1*n2)
+print("Division: ",n1/n2)
+print("Rest: ",n1%n2)
+
+print()
+print("Relational Operators")
+print("Equality: ",n1 == n2)
+print("Difference: ",n1 != n2)
+print("Less than: ",n1 < n2)
+print("Bigge then: ",n1 > n2)
+print("Less or equal: ",n1 <= n2)
+print("Bigge or equal: ",n1 >= n2)
+
+print()
+print("Logical Operators")
+print(aprovado)
+print(not(aprovado))
+print(n1 > 7 and minimo == 7)
+print(n1 > 7 or minimo == 7)
